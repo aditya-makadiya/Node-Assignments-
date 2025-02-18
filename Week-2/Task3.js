4135f6f8b4c2bb936fd76051e7d400118777698d
adityamakadiya <aditya.makadiya@bacancy.com>
2025-02-18 09:52:20 +0530

    Commit

--- Week-2/Task3.js
+++ Week-2/Task3.js
@@ -1,7 +1,38 @@
-function greet(){
-  console.log("hello");
-}
-greet();
-(function() {
-    console.log("Program started! This function runs only once.");
+//Create simple function that will run only once while we start program.
+
+// It can create by IIFE(Immediately Invoked Function Expressions) which is defined and invoked immediately after its declaration.
+
+
+(function(x, y){
+  let sum = x+y;
+  console.log(sum);
+  })(5,3); // output = 8
+
+
+
+var counter = (function (){
+  let count = 0;
+  return{
+    increment : function(){
+      count++;
+    },
+    decrement : function(){
+      count--;
+    },
+    getcount : function(){
+      return count;
+    }
+  };
 })();
+
+counter.increment();
+counter.increment();
+counter.increment();
+console.log(counter.getcount()); // output = 3
+counter.decrement();
+console.log(counter.getcount()); // output = 2
+console.log(count); // reference error
+
+// IIFE used to not pollute a globle scope by keeping variable and function private within its scope.
+
+
